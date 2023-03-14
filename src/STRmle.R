4e5289e1bbae5770c3090146ffe61fff71ef04de
ChristianTsoungui <christian.tsoungui@aims-cameroon.org>
2023-03-14 17:05:14 +0100

    updated

--- src/STRmle.R
+++ src/STRmle.R
@@ -19,19 +19,19 @@ DATA <- read.xlsx('/home/janedoe/Documents/datasets/example_dataset1.xlsx', 1)
 source("/home/janedoe/Documents/src/STRmodel.R")
 
 # Find MLEs (haplotype frequencies and MOI)
-mle(X, c(3,2), id = TRUE, plugin = NULL)
+mle(DATA, c(3,2), id = TRUE, plugin = NULL)
 
 # Find Haplotype frequencies assuming MOI known, i.e., lambda=0.2
-mle(X, c(3,2), id = TRUE, plugin = 0.2)
+mle(DATA, c(3,2), id = TRUE, plugin = 0.2)
 
 # Find MLEs (haplotype frequencies and MOI) with bootstrap bias-correction
-mle(X, c(3,2), id = TRUE, plugin = NULL, BC = TRUE, method = "bootstrap")
+mle(DATA, c(3,2), id = TRUE, plugin = NULL, BC = TRUE, method = "bootstrap")
 
 # Find MLEs (haplotype frequencies and MOI) using a 95% confidence interval
-mle(X, c(3,2), id = TRUE, plugin = NULL, CI = TRUE)
+mle(DATA, c(3,2), id = TRUE, plugin = NULL, CI = TRUE)
 
 # Find MLEs (haplotype frequencies and MOI) using a 90% confidence interval and 15000 bootstrap samples
-mle(X, c(3,2), id = TRUE, plugin = NULL, CI = TRUE, B = 15000, alpha = 0.1)
+mle(DATA, c(3,2), id = TRUE, plugin = NULL, CI = TRUE, B = 15000, alpha = 0.1)
 
 # Find LD between the two loci. The function outputs the LD measures D', r-squared, and Q-star.
-ldestim(X, c(3,2), id = TRUE, plugin = NULL, CI = TRUE, B = 15000, alpha = 0.1)
+ldestim(DATA, c(3,2), id = TRUE, plugin = NULL, CI = TRUE)
